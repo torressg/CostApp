e67711abbbe8ed04e98e4a5dd53fd59c50c2bdb8
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-06-03 09:51:30 -0300

    Add Delete of CRUD

--- costapp/src/components/pages/Projects.js
+++ costapp/src/components/pages/Projects.js
@@ -14,6 +14,7 @@ export function Projects(){
 
     const [projects, setProjects] = useState([])
     const [removeloading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -37,6 +38,21 @@ export function Projects(){
 
     }, [])
 
+    function removeProject(id){
+
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then((resp) => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(error => console.log(error))
+    }
+
     return(
         <div className={styles.projectcont}>
             <div className={styles.titlecont}>
@@ -44,6 +60,7 @@ export function Projects(){
                 <LinkButton to='/newproject' text='Criar projeto'/>
             </div>
             {message && <Message msg={message} type="success"/>}
+            {projectMessage && <Message msg={projectMessage} type="success"/>}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -53,6 +70,7 @@ export function Projects(){
                             budget={project.budget}
                             category={project.category.name}
                             key={project.id}
+                            handleRemove={removeProject}
                         />
                     ))}
                     {!removeloading && <Loading />}
